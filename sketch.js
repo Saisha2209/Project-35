2dfc458ec4f0c3f9c732196f5f7fdd8b54e85d06
Saisha2209 <76441200+Saisha2209@users.noreply.github.com>
2021-06-01 16:41:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,73 @@
+var balloon,balloonImage1,balloonImage2;
+// create database and position variable here
+var database,position
+function preload(){
+   bg =loadImage("cityImage.png");
+   balloonImage1=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+  }
+
+//Function to set initial environment
+function setup() {
+  database=firebase.database();
+  
+  balloonPosition=  database.ref('Balloon/height')
+  balloonPosition.on('value',readHeight,showError)
+  createCanvas(1500,700);
+  balloon=createSprite(250,450,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+
+  textSize(20); 
+}
+
+// function to display UI
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in left direction
+    changePosition(-20,0)
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in right direction
+    changePosition(+20,0)
+  }
+  else if(keyDown(UP_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in up direction
+    changePosition(0,-20)
+   balloon.scale=0.5
+  }
+  else if(keyDown(DOWN_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in down direction
+    changePosition(0,+20)
+   balloon.scale=1
+  }
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
+function readHeight(data){
+  
+    height=data.val();
+    balloon.x=height.x
+    balloon.y=height.y
+
+}
+function showError(){
+  console.log('There is an error in firebase database')
+}
+function changePosition(x,y){
+ balloon.x=balloon.x+x
+ balloon.y=balloon.y+y
+
+}
